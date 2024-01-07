4e911cf36ae64935cb5b82f1271db071db43d352
muna2411 <nurnahar.muna.2411@gmail.com>
2024-01-07 16:59:15 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const _ = require('underscore');
+
+const array = ["Java", "Javascript", "Typescript"];
+
+const a1 = _.contains(array, "Java"); 
+
+console.log(a1);
+
+
+const a2 = _.contains(array, "C++");  
+
+console.log(a2);
